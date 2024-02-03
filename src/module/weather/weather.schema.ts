10f34b206e2da05519c8e81e07fb5a7b7a54af3f
jessur2030 <jessusdev@gmail.com>
2024-02-03 15:10:15 -0500

    Add healthcheckResponseSchema to weather.schema.ts

--- src/module/weather/weather.schema.ts
+++ src/module/weather/weather.schema.ts
@@ -19,12 +19,20 @@ export const weatherErrorResponseSchema = z.object({
   message: z.string(),
 });
 
+export const healthcheckResponseSchema = z.object({
+  status: z.string(),
+  port: z.number(),
+  timestamp: z.string(),
+})
+
 export type WeatherSchemaType = z.infer<typeof weatherQuerySchema>;
 export type Weather200ResponseSchemaType = z.infer<typeof weather200ResponseSchema>;
+export type healthcheckResponseSchemaType = z.infer<typeof healthcheckResponseSchema>;
 
 // Exporting JSON Schemas for Fastify and Swagger
 export const weatherQueryJsonSchema = zodToJsonSchema(weatherQuerySchema);
 export const weather200ResponseJsonSchema = zodToJsonSchema(weather200ResponseSchema);
 export const weatherErrorResponseJsonSchema = zodToJsonSchema(weatherErrorResponseSchema);
+export const healthcheckResponseJsonSchema = zodToJsonSchema(healthcheckResponseSchema);
 
 
