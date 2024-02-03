085b23f82783814dabd137fa50c7d93e60277039
jessur2030 <jessusdev@gmail.com>
2024-02-03 15:02:03 -0500

    Refactor weather routes and add response schemas

--- src/module/weather/weather.route.ts
+++ src/module/weather/weather.route.ts
@@ -1,14 +1,26 @@
-import {FastifyInstance} from 'fastify';
+import { FastifyInstance } from 'fastify';
 import { currentWeatherDataHandler } from './weather.controller';
-import { WeatherSchemaJsonSchema } from './weather.schema';
+import { weatherQueryJsonSchema, weather200ResponseJsonSchema, weatherErrorResponseJsonSchema } from './weather.schema';
 
 export async function weatherRoutes(app: FastifyInstance) {
-    app.get('/current', 
-    {
-        schema: {
-            querystring: WeatherSchemaJsonSchema,
-          },
-      },
-    currentWeatherDataHandler)
-    app.get('/forecast', currentWeatherDataHandler)
+  app.get('/current', {
+    schema: {
+      querystring: weatherQueryJsonSchema,
+      response: {
+        200: weather200ResponseJsonSchema,
+        400: weatherErrorResponseJsonSchema,
+        500: weatherErrorResponseJsonSchema,
+      }
+    }
+  }, currentWeatherDataHandler);
+  app.get('/forecast', {
+    schema: {
+      querystring: weatherQueryJsonSchema,
+      response: {
+        200: weather200ResponseJsonSchema,
+        400: weatherErrorResponseJsonSchema,
+        500: weatherErrorResponseJsonSchema,
+      }
+    }
+  }, currentWeatherDataHandler);
 }
