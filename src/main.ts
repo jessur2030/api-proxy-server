2a96b7fc3dd7887fe38984de1ef894df65daab56
jessur2030 <jessusdev@gmail.com>
2024-02-03 15:10:09 -0500

    Add Swagger documentation and UI for API endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import fastify, { FastifyInstance } from "fastify";
 import fastifyCors from "@fastify/cors";
+import {fastifySwagger} from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 import { weatherRoutes } from './module/weather/weather.route';
+import { healthcheckResponseJsonSchema } from "./module/weather/weather.schema";
 
 const PORT = parseInt(process.env.PORT || "9000", 10);
 const HOST = process.env.HOST || "0.0.0.0";
@@ -15,21 +18,37 @@ const NODE_ENV = process.env.NODE_ENV;
 export async function buildServer(): Promise<FastifyInstance> {
     const app = fastify()
 
-     /**
-     * Healthcheck route.
-     * @returns {Object} The healthcheck response.
-     */
-    app.get('/healthcheck', ()=>{
+	await app.register(fastifySwagger, {
+        mode: "dynamic",
+		swagger: {
+            info: {
+                title: "Proxy server API",
+				version: "0.1.0",
+                description: "Proxy server API",
+
+			},
+		},
+
+
+	});
+
+	await app.register(fastifySwaggerUi, {
+		routePrefix: "/docs",
+
+	});
+
+    /**
+    * Healthcheck route.
+    * @returns {Object} The healthcheck response.
+    */
+   app.get('/healthcheck',  {schema: {response: {200: healthcheckResponseJsonSchema,},}}, ()=>{
         return {status: "Ok", port: PORT, timestamp: new Date().toISOString()}
     })
 
-    // register weather routes with prefix api/v1/weather
+    // // register weather routes with prefix api/v1/weather
     app.register(weatherRoutes, { prefix: '/api/v1/weather' })
 
-    // register cors
-    app.register(fastifyCors, {
-        origin: CORS_ORIGIN
-    })
+    // add schema for weather routes
     return app;
 }
 
