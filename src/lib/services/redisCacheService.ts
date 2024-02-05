eaef9ca889a352b8979ebeac043b1a05a30dd4c4
jessur2030 <jessusdev@gmail.com>
2024-02-05 11:13:41 -0500

    update redis caching service

--- src/lib/services/redisCacheService.ts
+++ src/lib/services/redisCacheService.ts
@@ -1,19 +1,38 @@
-// src/lib/services/CacheService.ts
 import redis from '../../config/redisConfig';
 
-class CacheService {
-    async get(key: string): Promise<string | null> {
+/**
+ * Provides caching services using Redis as the backend store.
+ */class CacheService {
+    /**
+     * Retrieves a value from Redis cache and parses it as JSON.
+     * 
+     * @param {string} key - The key to retrieve from the cache.
+     * @returns {Promise<any | null>} - The parsed JSON object associated with the key in the cache, or null if the key does not exist or an error occurs.
+     */
+    async get(key: string): Promise<any | null> {
         try {
-            return await redis.get(key);
+            const value = await redis.get(key);
+            return value ? JSON.parse(value) : null;
         } catch (error) {
             console.error(`Error getting key ${key} from Redis`, error);
             return null;
         }
     }
 
-    async set(key: string, value: string, ttl = 3600): Promise<void> {
+    /**
+     * Serializes an object to a JSON string and sets it in Redis cache with an optional time-to-live (TTL).
+     * 
+     * @param {string} key - The key under which to store the value.
+     * @param {any} value - The object to serialize and store in the cache.
+     * @param {number} [ttl=3600] - The time-to-live (TTL) in seconds. Defaults to 3600 seconds (1 hour).
+     * @returns {Promise<void>}
+     */
+    async set(key: string, value: any, ttl = 3600): Promise<void> {
+        console.log(`Setting key ${key} in Redis`, `Value: ${value}`, `TTL: ${ttl}`)
+
         try {
-            await redis.set(key, value, 'EX', ttl);
+            const stringValue = JSON.stringify(value);
+            await redis.set(key, stringValue, 'EX', ttl);
         } catch (error) {
             console.error(`Error setting key ${key} in Redis`, error);
         }
@@ -29,6 +48,5 @@ class CacheService {
 }
 
 // Export an instance of the CacheService
-const cacheService = new CacheService();
-export default cacheService;
-// 
+const redisCacheService = new CacheService();
+export default redisCacheService;
